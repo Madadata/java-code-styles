0c86e57c44a8e43f69591b784f4e5d842a9a0ea3
Jiayu Liu <jimexist@gmail.com>
2016-06-06 16:26:21 +0800

    renaming

--- install.bat
+++ install.bat
@@ -1,6 +1,6 @@
-REM Installs Square's IntelliJ configs into your user configs.
+REM Installs Mada's IntelliJ configs into your user configs.
 @echo off
-echo Installing Square IntelliJ configs...
+echo Installing Mada IntelliJ configs...
 
 setlocal enableDelayedExpansion
 
@@ -9,7 +9,7 @@ for /D %%i in (%userprofile%\.IdeaIC*) do call :copy_config %%i
 for /D %%i in (%userprofile%\.IntelliJIdea*) do call :copy_config %%i
 
 echo.
-echo Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Square' or 'SquareAndroid'.
+echo Restart IntelliJ and/or AndroidStudio, go to preferences, and apply 'Mada' or 'MadaAndroid'.
 exit /b
 
 REM sub function for copy config files
